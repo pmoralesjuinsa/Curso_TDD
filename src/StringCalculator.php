1de5246899f6f5cfd90e2d8c19659f94b1c5968c
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-03 09:56:52 +0100

    Inicio Proyecto

---
+++ src/StringCalculator.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Src;
+
+
+class StringCalculator
+{
+    public function Add($numbers)
+    {
+        return $numbers;
+    }
+}
---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TDD\Tests;
+
+
+use Src\StringCalculator;
+
+class StringCalculatorTest extends \PHPUnit\Framework\TestCase
+{
+
+    /** @test */
+    public function send_string_numbers_separated_commas_and_return_their_sum()
+    {
+        $calculator = new StringCalculator();
+        $result = $calculator->Add("1,2");
+
+        $this->assertEquals(3, $result);
+    }
+
+
+}
+
+
