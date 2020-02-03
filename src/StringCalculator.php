b68246acede3ca7578c36fde727e5db7a233c5af
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-03 10:00:10 +0100

    RED: Send 1,2 as string and fail return 3 as int

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -8,6 +8,13 @@ class StringCalculator
 {
     public function Add($numbers)
     {
+        $total = 0;
+        $numbersArray = explode(",", $numbers);
+        if(!empty($numbersArray)) {
+            foreach($numbersArray as $number) {
+                $total += $number;
+            }
+        }
         return $numbers;
     }
 }
