8c205773efd39022ec97f2f4564d262da9f91daa
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-04 15:40:04 +0100

    GREEN: send five return Buzz

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -6,6 +6,8 @@ namespace Src;
 class FizzBuzz
 {
     CONST FIZZ_NUMBER = 3;
+    CONST BUZZ_NUMBER = 5;
+
     protected $databaseFake;
 
     public  function __construct($databaseFake)
@@ -28,6 +30,10 @@ class FizzBuzz
 
         if($number%self::FIZZ_NUMBER === 0) {
             $result = $this->databaseFake->getStringWhenThreeNumber();
+        }
+
+        if($number%self::BUZZ_NUMBER === 0) {
+            $result = "Buzz";
         }
 
         return $result;
