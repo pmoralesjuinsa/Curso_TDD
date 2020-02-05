9eb53e2749a8a2a4554e671052e87ba7230a7757
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-05 17:03:34 +0100

    GREEN: send 2 return II

--- src/RomanNumeral.php
+++ src/RomanNumeral.php
@@ -9,13 +9,28 @@ class RomanNumeral
 
     public function getRomanNumber($number)
     {
+        $letter = $this->convertNumberToLetter($number);
+
+        return $letter;
+    }
+
+    /**
+     * @param $number
+     * @return string
+     */
+    public function convertNumberToLetter($number): string
+    {
         $letter = '';
 
-        if($number == 1) {
+        if ($number % 1 === 0) {
             $letter = "I";
         }
 
-        if($number == 5) {
+        if ($number % 2 === 0) {
+            $letter = "II";
+        }
+
+        if($number % 5 === 0) {
             $letter = "V";
         }
 
