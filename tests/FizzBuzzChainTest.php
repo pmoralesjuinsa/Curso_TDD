2d591af71f3ed3aa77c79be01540ca40db5d1353
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-07 10:01:42 +0100

    BLUE: refactor tests

--- tests/FizzBuzzChainTest.php
+++ tests/FizzBuzzChainTest.php
@@ -11,12 +11,17 @@ use Src\FizzBuzzChain;
 class FizzBuzzChainTest extends TestCase
 {
 
+    protected $fizzBuzzChain;
+
+    function setUp(): void
+    {
+        $this->fizzBuzzChain = new FizzBuzzChain();
+    }
+
     /** @test */
     public function send_one_return_one()
     {
-        $fizzBuzzChain = new FizzBuzzChain();
-
-        $result = $fizzBuzzChain->passNumber(1);
+        $result = $this->fizzBuzzChain->passNumber(1);
 
         $this->assertEquals(1, $result);
 
@@ -25,25 +30,27 @@ class FizzBuzzChainTest extends TestCase
     /** @test */
     public function send_a_return_exception()
     {
-        $fizzBuzzChain = new FizzBuzzChain();
-
         $number = "a";
 
         $this->expectExceptionMessage("$number no es un número");
 
-        $fizzBuzzChain->passNumber($number);
+        $this->fizzBuzzChain->passNumber($number);
 
     }
 
     /** @test */
     public function send_three_return_fizz()
     {
-        $fizzBuzzChain = new FizzBuzzChain();
-
-        $result = $fizzBuzzChain->passNumber(3);
+        $result = $this->fizzBuzzChain->passNumber(3);
 
         $this->assertEquals("Fizz", $result);
 
     }
+
+//    /** @test */
+//    public function send_five_return_buzz()
+//    {
+//
+//    }
 
 }
